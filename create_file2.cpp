75982b2d8ce6b87eb7a5d6ac11bc56cc9e29afe7
FlyingDom <82442247+sung-yong-k@users.noreply.github.com>
2026-01-09 09:52:19 +0100

    Create create_file2.cpp

---
+++ create_file2.cpp
@@ -0,0 +1,22 @@
+﻿#include <fstream>
+#include <string>
+
+void tryCreate(const std::string& path, const std::string& content)
+{
+    std::ofstream file(path);
+    if (!file.is_open())
+        return; // Path or drive doesn't exist → ignore
+
+    file << content;
+    file.close();
+}
+
+int main()
+{
+    tryCreate("C:\\test2.txt", "testC");
+    tryCreate("X:\\test2.txt", "testX");
+    tryCreate("D:\\test2.txt", "testD");
+    tryCreate("G:\\test2.txt", "testG");
+
+    return 0;
+}
